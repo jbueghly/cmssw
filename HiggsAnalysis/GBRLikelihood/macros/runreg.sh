011b0362e9f72e0f2c8bd089e34a2458acd0e0d5
James Bueghly <jamesbueghly2015@u.northwestern.edu>
2019-05-09 08:58:38 -0500

    removing unneeded GBRLikelihood

--- HiggsAnalysis/GBRLikelihood/macros/runreg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-root -l -q $CMSSW_BASE/src/HiggsAnalysis/GBRLikelihood/macros/eregtraining.C+\(true,false\)
-root -l -q $CMSSW_BASE/src/HiggsAnalysis/GBRLikelihood/macros/eregtraining.C+\(false,false\)
-root -l -q $CMSSW_BASE/src/HiggsAnalysis/GBRLikelihood/macros/eregtraining.C+\(true,true\)
-root -l -q $CMSSW_BASE/src/HiggsAnalysis/GBRLikelihood/macros/eregtraining.C+\(false,true\)
