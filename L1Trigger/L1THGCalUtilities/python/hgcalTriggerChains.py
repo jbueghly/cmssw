7ead62ead71a403333c1e08dca1c3a4f2fe98705
James Bueghly <jamesbueghly2015@u.northwestern.edu>
2019-06-26 16:36:19 -0500

    adding calo truth TPG chains

--- L1Trigger/L1THGCalUtilities/python/hgcalTriggerChains.py
+++ L1Trigger/L1THGCalUtilities/python/hgcalTriggerChains.py
@@ -4,17 +4,23 @@ class HGCalTriggerChains:
     def __init__(self):
         self.vfe = {}
         self.concentrator = {}
+        self.calo = {}
         self.backend1 = {}
         self.backend2 = {}
         self.selector = {}
         self.ntuple = {}
         self.chain = []
+        self.truth_chain = []
+        self.truth_be_chain = []
 
     def register_vfe(self, name, generator):
         self.vfe[name] = generator
 
     def register_concentrator(self, name, generator):
         self.concentrator[name] = generator
+
+    def register_calo(self, name, generator): # for truth-matched trigger cells
+        self.calo[name] = generator
 
     def register_backend1(self, name, generator):
         self.backend1[name] = generator
@@ -42,7 +48,28 @@ class HGCalTriggerChains:
         if ntuple!='' and not ntuple in self.ntuple: 
             raise KeyError('{} not registered as ntuplizer'.format(ntuple))
         self.chain.append( (vfe, concentrator, backend1, backend2, selector, ntuple) )
-    
+
+    def register_truth_chain(self, vfe, calo, selector='', ntuple=''):
+        if not vfe in self.vfe: 
+            raise KeyError('{} not registered as VFE producer'.format(vfe))
+        if not calo in self.calo: 
+            raise KeyError('{} not registered as calo producer'.format(calo))
+        self.truth_chain.append( (vfe, calo, selector, ntuple) )
+
+    def register_truth_be_chain(self, vfe, calo, backend1, backend2, selector='', ntuple=''): 
+        if not vfe in self.vfe: 
+            raise KeyError('{} not registered as VFE producer'.format(vfe))
+        if not calo in self.calo: 
+            raise KeyError('{} not registered as calo producer'.format(calo))
+        if not backend1 in self.backend1: 
+            raise KeyError('{} not registered as backend1 producer'.format(backend1))
+        if not backend2 in self.backend2: 
+            raise KeyError('{} not registered as backend2 producer'.format(backend2))
+        if selector!='' and not selector in self.selector:
+            raise KeyError('{} not registered as selector'.format(selector))
+        if ntuple!='' and not ntuple in self.ntuple: 
+            raise KeyError('{} not registered as ntuplizer'.format(ntuple))
+        self.truth_be_chain.append( (vfe, calo, backend1, backend2, selector, ntuple) )
 
     def create_sequences(self, process):
         if not hasattr(process, 'hgcalTriggerSelector'):
@@ -50,6 +77,7 @@ class HGCalTriggerChains:
         tmp = cms.SequencePlaceholder("tmp")
         vfe_sequence = cms.Sequence(tmp)
         concentrator_sequence = cms.Sequence(tmp)
+        calo_sequence = cms.Sequence(tmp)
         backend1_sequence = cms.Sequence(tmp)
         backend2_sequence = cms.Sequence(tmp)
         selector_sequence = cms.Sequence(tmp)
@@ -89,7 +117,72 @@ class HGCalTriggerChains:
             if ntuple!='' and not hasattr(process, ntuple_name):
                 setattr(process, ntuple_name, self.ntuple[ntuple](process, ntuple_inputs))
                 ntuple_sequence *= getattr(process, ntuple_name)
+        for vfe,calo,selector,ntuple in self.truth_chain:
+            calo_name = '{0}{1}'.format(vfe, calo)
+            ntuple_name = '{0}{1}{2}'.format(vfe, calo, ntuple)
+            if selector!='' and not hasattr(process, selector_name):
+                setattr(process, selector_name, self.selector[selector](process, calo_name))
+                selector_sequence *= getattr(process, selector_name)
+            if selector=='':
+                ntuple_inputs = [calo_name,
+                            '',
+                            calo_name
+                            ]
+            else:
+                ntuple_inputs = [
+                        calo_name,
+                        '',
+                        selector_name]
+            if not hasattr(process, vfe):
+                setattr(process, vfe, self.vfe[vfe](process))
+                vfe_sequence *= getattr(process, vfe)
+            if not hasattr(process, calo_name):
+                setattr(process, calo_name, self.calo[calo](process, vfe))
+                calo_sequence *= getattr(process, calo_name)
+            if selector!='' and not hasattr(process, selector_name):
+                setattr(process, selector_name, self.selector[selector](process, backend2_name))
+                selector_sequence *= getattr(process, selector_name)
+            if ntuple!='' and not hasattr(process, ntuple_name):
+                setattr(process, ntuple_name, self.ntuple[ntuple](process, ntuple_inputs))
+                ntuple_sequence *= getattr(process, ntuple_name)
+        for vfe,calo,backend1,backend2,selector,ntuple in self.truth_be_chain:
+            calo_name = '{0}{1}'.format(vfe, calo)
+            backend1_name = '{0}{1}{2}'.format(vfe, calo, backend1)
+            backend2_name = '{0}{1}{2}{3}'.format(vfe, calo, backend1, backend2)
+            selector_name = '{0}{1}{2}{3}{4}'.format(vfe, calo, backend1, backend2, selector)
+            ntuple_name = '{0}{1}{2}{3}{4}{5}'.format(vfe, calo, backend1, backend2, selector, ntuple)
+            if selector=='':
+                ntuple_inputs = [
+                        calo_name,
+                        backend1_name+':HGCalBackendLayer1Processor2DClustering',
+                        backend2_name+':HGCalBackendLayer2Processor3DClustering'
+                        ]
+            else:
+                ntuple_inputs = [
+                        calo_name,
+                        backend1_name+':HGCalBackendLayer1Processor2DClustering',
+                        selector_name]
+            if not hasattr(process, vfe):
+                setattr(process, vfe, self.vfe[vfe](process))
+                vfe_sequence *= getattr(process, vfe)
+            if not hasattr(process, calo_name):
+                setattr(process, calo_name, self.calo[calo](process, vfe))
+                calo_sequence *= getattr(process, calo_name)
+            if not hasattr(process, backend1_name):
+                setattr(process, backend1_name, self.backend1[backend1](process, calo_name))
+                backend1_sequence *= getattr(process, backend1_name)
+            if not hasattr(process, backend2_name):
+                setattr(process, backend2_name, self.backend2[backend2](process, backend1_name))
+                backend2_sequence *= getattr(process, backend2_name)
+            if selector!='' and not hasattr(process, selector_name):
+                setattr(process, selector_name, self.selector[selector](process, backend2_name))
+                selector_sequence *= getattr(process, selector_name)
+            if ntuple!='' and not hasattr(process, ntuple_name):
+                setattr(process, ntuple_name, self.ntuple[ntuple](process, ntuple_inputs))
+                ntuple_sequence *= getattr(process, ntuple_name)
+
         vfe_sequence.remove(tmp)
+        calo_sequence.remove(tmp)
         concentrator_sequence.remove(tmp)
         backend1_sequence.remove(tmp)
         backend2_sequence.remove(tmp)
@@ -97,6 +190,7 @@ class HGCalTriggerChains:
         ntuple_sequence.remove(tmp)
         process.globalReplace('hgcalVFE', vfe_sequence)
         process.globalReplace('hgcalConcentrator', concentrator_sequence)
+        process.globalReplace('caloTruthCells', calo_sequence)
         process.globalReplace('hgcalBackEndLayer1', backend1_sequence)
         process.globalReplace('hgcalBackEndLayer2', backend2_sequence)
         process.globalReplace('hgcalTriggerSelector', selector_sequence)
